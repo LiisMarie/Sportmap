758fa1982f9cf1a0f0cfee88991d89662864b0b3
likutt <likutt@ttu.ee>
2020-03-26 12:20:02 +0200

    initial commit

---
+++ app/src/main/java/ee/taltech/likutt/iti0213_2019s_hw02/C.kt
@@ -0,0 +1,19 @@
+package ee.taltech.likutt.iti0213_2019s_hw02
+
+class C {
+    companion object {
+        val NOTIFICATION_CHANNEL = "default_channel"
+        val NOTIFICATION_ACTION_WP = "ee.taltech.likutt.wp"
+        val NOTIFICATION_ACTION_CP = "ee.taltech.likutt.cp"
+
+        val LOCATION_UPDATE_ACTION = "ee.taltech.likutt.location_update"
+
+        val LOCATION_UPDATE_ACTION_LATITUDE = "ee.taltech.likutt.location_update.latitude"
+        val LOCATION_UPDATE_ACTION_LONGITUDE = "ee.taltech.likutt.location_update.longitude"
+
+        val NOTIFICATION_ID = 4321
+        val REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
+
+    }
+}
