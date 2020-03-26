06440073ceda70c6990bc5409fd49091f5cbcd02
likutt <likutt@ttu.ee>
2020-03-26 22:37:50 +0200

    added compass

--- app/src/main/java/ee/taltech/likutt/iti0213_2019s_hw02/MainActivity.kt
+++ app/src/main/java/ee/taltech/likutt/iti0213_2019s_hw02/MainActivity.kt
@@ -12,13 +12,16 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.view.View
-import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -31,9 +34,16 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.track_control.*
+import android.hardware.Sensor.TYPE_ACCELEROMETER
+import android.hardware.Sensor.TYPE_MAGNETIC_FIELD
+import android.hardware.SensorManager.SENSOR_DELAY_GAME
+import android.view.animation.Animation.RELATIVE_TO_SELF
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
+import java.lang.Math.toDegrees
 
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
     companion object {
         private val TAG = this::class.java.declaringClass!!.simpleName
     }
@@ -52,7 +62,18 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private val startButtonColor = Color.parseColor("#9ccc65")
     private val stopButtonColor = Color.parseColor("#e57373")
 
+    // compass start
+    lateinit var sensorManager: SensorManager
+    lateinit var image: ImageView
+    lateinit var accelerometer: Sensor
+    lateinit var magnetometer: Sensor
 
+    var currentDegree = 0.0f
+    var lastAccelerometer = FloatArray(3)
+    var lastMagnetometer = FloatArray(3)
+    var lastAccelerometerSet = false
+    var lastMagnetometerSet = false
+    //compass end
 
     // ============================================== MAIN ENTRY - ONCREATE =============================================
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -79,6 +100,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         imageButtonWP.setOnClickListener {buttonWPOnClick()}
 
         buttonStartStop.setBackgroundColor(startButtonColor)
+
+        // compass
+        image = findViewById(R.id.imageViewCompass) as ImageView
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(TYPE_ACCELEROMETER)
+        magnetometer = sensorManager.getDefaultSensor(TYPE_MAGNETIC_FIELD)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -108,11 +135,22 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, broadcastReceiverIntentFilter)
 
+        // compass
+        if (compassOn) {
+            sensorManager.registerListener(this, accelerometer, SENSOR_DELAY_GAME)
+            sensorManager.registerListener(this, magnetometer, SENSOR_DELAY_GAME)
+        }
     }
 
     override fun onPause() {
         Log.d(TAG, "onPause")
         super.onPause()
+
+        // compass
+        if (compassOn) {
+            sensorManager.unregisterListener(this, accelerometer)
+            sensorManager.unregisterListener(this, magnetometer)
+        }
     }
 
     override fun onStop() {
@@ -279,7 +317,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     fun buttonCenteredOnClick(view: View) {
-        Log.d(TAG, "buttonCenteredOnClick")
+        Log.d(TAG, "buttonCenteredOnClick " + buttonCentered.text)
         if (!mapCentered) {
             buttonCentered.text = "Centered"
             mapCentered = true
@@ -290,7 +328,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     fun buttonDirectionOnClick(view: View) {
-        Log.d(TAG, "buttonDirectionOnClick")
+        Log.d(TAG, "buttonDirectionOnClick " + buttonDirection.text)
         // todo logic
         if (buttonDirection.text == "North-up") {
             buttonDirection.text = "Direction up"
@@ -302,15 +340,20 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     fun buttonCompassOnClick(view: View) {
-        Log.d(TAG, "buttonCompassOnClick")
+        Log.d(TAG, "buttonCompassOnClick " + compassOn)
         // todo logic
         if (compassOn) {
             compassOn = false
             imageButton.setImageResource(R.drawable.baseline_explore_off_white_24)
+            sensorManager.unregisterListener(this, accelerometer)
+            sensorManager.unregisterListener(this, magnetometer)
+            includeCompass.visibility = View.INVISIBLE
         } else {
             compassOn = true
             imageButton.setImageResource(R.drawable.baseline_explore_white_24)
-
+            sensorManager.registerListener(this, accelerometer, SENSOR_DELAY_GAME)
+            sensorManager.registerListener(this, magnetometer, SENSOR_DELAY_GAME)
+            includeCompass.visibility = View.VISIBLE
         }
     }
 
@@ -340,6 +383,55 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
                 }
             }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.d(TAG, "onAccuracyChanged")
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        Log.d(TAG, "onSensorChanged")
+        if (compassOn) {
+            if (event != null) {
+                if (event.sensor === accelerometer) {
+                    lowPass(event.values, lastAccelerometer)
+                    lastAccelerometerSet = true
+                } else if (event.sensor === magnetometer) {
+                    lowPass(event.values, lastMagnetometer)
+                    lastMagnetometerSet = true
+                }
+            }
+
+            if (lastAccelerometerSet && lastMagnetometerSet) {
+                val r = FloatArray(9)
+                if (SensorManager.getRotationMatrix(r, null, lastAccelerometer, lastMagnetometer)) {
+                    val orientation = FloatArray(3)
+                    SensorManager.getOrientation(r, orientation)
+                    val degree = (toDegrees(orientation[0].toDouble()) + 360).toFloat() % 360
+
+                    val rotateAnimation = RotateAnimation(
+                        currentDegree,
+                        -degree,
+                        RELATIVE_TO_SELF, 0.5f,
+                        RELATIVE_TO_SELF, 0.5f)
+                    rotateAnimation.duration = 1000
+                    rotateAnimation.fillAfter = true
+
+                    image.startAnimation(rotateAnimation)
+                    currentDegree = -degree
+                }
+            }
+        }
+
+
+    }
+
+    fun lowPass(input: FloatArray, output: FloatArray) {
+        val alpha = 0.05f
+
+        for (i in input.indices) {
+            output[i] = output[i] + alpha * (input[i] - output[i])
         }
     }
 
